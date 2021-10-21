ab5987c81e42e2e3eb192b210ad7a91b36d34718
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2021-10-21 19:41:15 -0400

    drivers/sensors: ALSPSsensor: Suppress more log spam
    This wouldn't be an issue if the driver didn't update its
    status every few milliseconds, that too with pretty useless
    information:
    
    10-21 19:19:08.828     0     0 I         : [ASH_ALGO][ALSPSsensor][mALSPS_algo_suspend]Driver SUSPEND +++
    10-21 19:19:08.828     0     0 I         : [ASH_ALGO][ALSPSsensor][mALSPS_algo_suspend]Driver SUSPEND ---
    
    Suppress it as usual.

--- drivers/sensors/ASH/ASH_Algo/ALSPSsensor.c
+++ drivers/sensors/ASH/ASH_Algo/ALSPSsensor.c
@@ -41,7 +41,7 @@
 #else
 	#define dbg(fmt, args...)
 #endif
-#define log(fmt, args...) printk(KERN_INFO "[%s][%s][%s]"fmt,MODULE_NAME,SENSOR_TYPE_NAME,__func__,##args)
+#define log(fmt, args...) pr_debug(KERN_INFO "[%s][%s][%s]"fmt,MODULE_NAME,SENSOR_TYPE_NAME,__func__,##args)
 #define err(fmt, args...) do{	\
 		printk(KERN_ERR "[%s][%s]"fmt,MODULE_NAME,SENSOR_TYPE_NAME,##args);	\
 		sprintf(g_error_mesg, "[%s][%s]"fmt,MODULE_NAME,SENSOR_TYPE_NAME,##args);	\

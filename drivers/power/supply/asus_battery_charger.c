ZenFone8/android_kernel_asus_sm8350

power: supply: qti: add pmic glink client
Change-Id: I1bb1ec7e95a286ebd2ae51300d77e51b0fd552bc

drivers/power/supply/asus_battery_charger.c
@@ -2,8 +2,150 @@
 
 #include "asus_battery_charger.h"
 
+#define MSG_OWNER_OEM			32782
+#define MSG_TYPE_REQ_RESP		1
+#define MSG_TYPE_NOTIFY			2
+
+#define OEM_OPCODE_WRITE_BUFFER		0x10001
+
+#define OEM_PROPERTY_MAX_DATA_SIZE	16
+
+struct oem_write_buffer_req_msg {
+	struct pmic_glink_hdr hdr;
+	u32 oem_property_id;
+	u32 data_buffer[OEM_PROPERTY_MAX_DATA_SIZE];
+	u32 data_size;
+};
+
+struct oem_write_buffer_resp_msg {
+	struct pmic_glink_hdr hdr;
+	u32 oem_property_id;
+	u32 return_status;
+};
+
+static int write_property_id_oem(struct asus_battery_chg *abc, u32 prop_id,
+				 u32 *buf, size_t count)
+{
+	struct oem_write_buffer_req_msg req_msg = { { 0 } };
+
+	req_msg.hdr.owner = MSG_OWNER_OEM;
+	req_msg.hdr.type = MSG_TYPE_REQ_RESP;
+	req_msg.hdr.opcode = OEM_OPCODE_WRITE_BUFFER;
+	req_msg.oem_property_id = prop_id;
+	req_msg.data_size = count;
+
+	memcpy(req_msg.data_buffer, buf, sizeof(u32) * count);
+
+	return battery_chg_write(abc->bcdev, &req_msg, sizeof(req_msg));
+}
+
+#define CHECK_LENGTH(msg)						\
+	if (len != sizeof(*msg)) {					\
+		dev_err(dev, "Bad response length %zu for opcode %u\n",	\
+		       len, hdr->opcode);				\
+		break;							\
+	}								\
+
+static void handle_notification(struct asus_battery_chg *abc, void *data,
+				size_t len)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct pmic_glink_hdr *hdr = data;
+
+	switch (hdr->opcode) {
+	default:
+		dev_err(dev, "Unknown opcode: %u\n", hdr->opcode);
+		break;
+	}
+}
+
+static void handle_message(struct asus_battery_chg *abc, void *data,
+			   size_t len)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct oem_write_buffer_resp_msg *resp_msg;
+	struct pmic_glink_hdr *hdr = data;
+	bool ack_set = false;
+
+	switch (hdr->opcode) {
+	case OEM_OPCODE_WRITE_BUFFER:
+		CHECK_LENGTH(resp_msg);
+
+		resp_msg = data;
+
+		switch (resp_msg->oem_property_id) {
+		default:
+			ack_set = true;
+			dev_err(dev, "Unknown property_id: %u\n",
+			       resp_msg->oem_property_id);
+			break;
+		}
+		break;
+	default:
+		dev_err(dev, "Unknown opcode: %u\n", hdr->opcode);
+		ack_set = true;
+		break;
+	}
+
+	if (ack_set)
+		battery_chg_complete_ack(abc->bcdev);
+}
+
+static void pmic_glink_state_cb(void *priv, enum pmic_glink_state state)
+{
+}
+
+static int pmic_glink_cb(void *priv, void *data, size_t len)
+{
+	struct asus_battery_chg *abc = priv;
+	struct pmic_glink_hdr *hdr = data;
+
+	if (!initialized || hdr->owner != MSG_OWNER_OEM)
+		return 0;
+
+	if (hdr->type == MSG_TYPE_NOTIFY)
+		handle_notification(abc, data, len);
+	else
+		handle_message(abc, data, len);
+
+	return 0;
+}
+
+void unregister_pmic_glink_client(void *data)
+{
+	struct pmic_glink_client *client = data;
+
+	pmic_glink_unregister_client(client);
+}
+
 int asus_battery_charger_init(struct asus_battery_chg *abc)
 {
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct pmic_glink_client_data client_data = { };
+	int rc;
+
+	client_data.id = MSG_OWNER_OEM;
+	client_data.name = "asus_BC";
+	client_data.priv = abc;
+	client_data.msg_cb = pmic_glink_cb;
+	client_data.state_cb = pmic_glink_state_cb;
+
+	abc->client = pmic_glink_register_client(dev, &client_data);
+	if (IS_ERR(abc->client)) {
+		rc = PTR_ERR(abc->client);
+		if (rc != -EPROBE_DEFER)
+			dev_err(dev, "Error in registering with pmic_glink %d\n",
+				rc);
+		return rc;
+	}
+
+	rc = devm_add_action_or_reset(dev, unregister_pmic_glink_client,
+				      &abc->client);
+	if (rc)
+		return rc;
+
+	abc->initialized = true;
+
 	return 0;
 }
 

sourajitk/msm-5.4

drivers/power/supply/asus_battery_charger.c
@@ -2,8 +2,150 @@
 
 #include "asus_battery_charger.h"
 
+#define MSG_OWNER_OEM			32782
+#define MSG_TYPE_REQ_RESP		1
+#define MSG_TYPE_NOTIFY			2
+
+#define OEM_OPCODE_WRITE_BUFFER		0x10001
+
+#define OEM_PROPERTY_MAX_DATA_SIZE	16
+
+struct oem_write_buffer_req_msg {
+	struct pmic_glink_hdr hdr;
+	u32 oem_property_id;
+	u32 data_buffer[OEM_PROPERTY_MAX_DATA_SIZE];
+	u32 data_size;
+};
+
+struct oem_write_buffer_resp_msg {
+	struct pmic_glink_hdr hdr;
+	u32 oem_property_id;
+	u32 return_status;
+};
+
+static int write_property_id_oem(struct asus_battery_chg *abc, u32 prop_id,
+				 u32 *buf, size_t count)
+{
+	struct oem_write_buffer_req_msg req_msg = { { 0 } };
+
+	req_msg.hdr.owner = MSG_OWNER_OEM;
+	req_msg.hdr.type = MSG_TYPE_REQ_RESP;
+	req_msg.hdr.opcode = OEM_OPCODE_WRITE_BUFFER;
+	req_msg.oem_property_id = prop_id;
+	req_msg.data_size = count;
+
+	memcpy(req_msg.data_buffer, buf, sizeof(u32) * count);
+
+	return battery_chg_write(abc->bcdev, &req_msg, sizeof(req_msg));
+}
+
+#define CHECK_LENGTH(msg)						\
+	if (len != sizeof(*msg)) {					\
+		dev_err(dev, "Bad response length %zu for opcode %u\n",	\
+		       len, hdr->opcode);				\
+		break;							\
+	}								\
+
+static void handle_notification(struct asus_battery_chg *abc, void *data,
+				size_t len)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct pmic_glink_hdr *hdr = data;
+
+	switch (hdr->opcode) {
+	default:
+		dev_err(dev, "Unknown opcode: %u\n", hdr->opcode);
+		break;
+	}
+}
+
+static void handle_message(struct asus_battery_chg *abc, void *data,
+			   size_t len)
+{
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct oem_write_buffer_resp_msg *resp_msg;
+	struct pmic_glink_hdr *hdr = data;
+	bool ack_set = false;
+
+	switch (hdr->opcode) {
+	case OEM_OPCODE_WRITE_BUFFER:
+		CHECK_SET_DATA(resp_msg);
+
+		resp_msg = data;
+
+		switch (resp_msg->oem_property_id) {
+		default:
+			ack_set = true;
+			dev_err(dev, "Unknown property_id: %u\n",
+			       resp_msg->oem_property_id);
+			break;
+		}
+		break;
+	default:
+		dev_err(dev, "Unknown opcode: %u\n", hdr->opcode);
+		ack_set = true;
+		break;
+	}
+
+	if (ack_set)
+		battery_chg_complete_ack(abc->bcdev);
+}
+
+static void pmic_glink_state_cb(void *priv, enum pmic_glink_state state)
+{
+}
+
+static int pmic_glink_cb(void *priv, void *data, size_t len)
+{
+	struct asus_battery_chg *abc = priv;
+	struct pmic_glink_hdr *hdr = data;
+
+	if (!abc->initialized || hdr->owner != MSG_OWNER_OEM)
+		return 0;
+
+	if (hdr->type == MSG_TYPE_NOTIFY)
+		handle_notification(abc, data, len);
+	else
+		handle_message(abc, data, len);
+
+	return 0;
+}
+
+void unregister_pmic_glink_client(void *data)
+{
+	struct pmic_glink_client *client = data;
+
+	pmic_glink_unregister_client(client);
+}
+
 int asus_battery_charger_init(struct asus_battery_chg *abc)
 {
+	struct device *dev = battery_chg_device(abc->bcdev);
+	struct pmic_glink_client_data client_data = { };
+	int rc;
+
+	client_data.id = MSG_OWNER_OEM;
+	client_data.name = "asus_BC";
+	client_data.priv = abc;
+	client_data.msg_cb = pmic_glink_cb;
+	client_data.state_cb = pmic_glink_state_cb;
+
+	abc->client = pmic_glink_register_client(dev, &client_data);
+	if (IS_ERR(abc->client)) {
+		rc = PTR_ERR(abc->client);
+		if (rc != -EPROBE_DEFER)
+			dev_err(dev, "Error in registering with pmic_glink %d\n",
+				rc);
+		return rc;
+	}
+
+	rc = devm_add_action_or_reset(dev, unregister_pmic_glink_client,
+				      &abc->client);
+	if (rc)
+		return rc;
+
+	abc->initialized = true;
+
 	return 0;
 }
 
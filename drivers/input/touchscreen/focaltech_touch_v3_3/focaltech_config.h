ZenFone8/android_kernel_asus_sm8350

input: touchscreen: focaltech_touch_v3_3: use different firmware update file for sake
Change-Id: Ia20865ce2fc2fbef30e1cf6e5fd94f9ccad77077

drivers/input/touchscreen/focaltech_touch_v3_3/focaltech_config.h
@@ -289,7 +289,12 @@
  * define your own fw_file, the sample one to be replaced is invalid
  * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_VENDOR_ID
  */
+#if defined ASUS_SAKE_PROJECT
+#define FTS_UPGRADE_FW_FILE                                                    \
+	"include/firmware/FT3658G_SAKE_V84_D01_20211103_app.i"
+#else
 #define FTS_UPGRADE_FW_FILE "include/firmware/fw_sample.i"
+#endif
 
 /*
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2

sourajitk/msm-5.4

drivers/input/touchscreen/focaltech_touch_v3_3/focaltech_config.h
@@ -289,7 +289,12 @@
  * define your own fw_file, the sample one to be replaced is invalid
  * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_VENDOR_ID
  */
+#if defined ASUS_SAKE_PROJECT
+#define FTS_UPGRADE_FW_FILE                                                    \
+	"include/firmware/FT3658G_SAKE_V85_D01_20211221_app.i"
+#else
 #define FTS_UPGRADE_FW_FILE "include/firmware/fw_sample.i"
+#endif
 
 /*
  * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2